7c34204f48de3c6b5833b94123fa3be46b9aeff0
NathnaelDS <nathnaelds@gmail.com>
2019-04-24 14:11:58 +0300

    Validation and styling file upload

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -3,11 +3,23 @@ import React from "react";
 export default class FileUpload extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      validFileType: true
+    };
     this.handleUpload = this.handleUpload.bind(this);
   }
 
   handleUpload(e) {
     let files = e.target.files;
+    if (files[0].name.endsWith(".json")) {
+      this.setState({
+        validFileType: true
+      });
+    } else {
+      this.setState({
+        validFileType: false
+      });
+    }
     let reader = new FileReader();
 
     reader.onload = function(e) {
@@ -22,15 +34,38 @@ export default class FileUpload extends React.Component {
 
   render() {
     return (
-      <label>
-        Data:
+      <div
+        style={{
+          fontSize: 14,
+          padding: 20,
+          marginLeft: 5,
+          marginTop: 20,
+          backgroundColor: "hsla(204, 30%, 93%, 1)",
+          width: 260,
+          height: 100,
+          display: "inline-block",
+          borderRadius: 3,
+          boxShadow: "0 1px 3px hsla(0, 0%, 0%, 0.3)"
+        }}
+      >
         <input
           accept=".json"
           onChange={e => this.handleUpload(e)}
           type="file"
           id="files"
         />
-      </label>
+        {!this.state.validFileType && (
+          <span
+            style={{
+              color: "red",
+              paddingTop: 12,
+              display: "inline-block"
+            }}
+          >
+            "The file format must be .json"
+          </span>
+        )}
+      </div>
     );
   }
 }
