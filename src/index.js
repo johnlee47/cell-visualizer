e823f198190b1b4210786985bc9730595247697a
NathnaelDS <nathnaelds@gmail.com>
2019-05-16 14:42:08 +0300

    Download report as PDF

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ import "antd/dist/antd.css";
 import "./style.css";
 import { AutoComplete } from "antd";
 import * as bg from "./home.svg";
+import html2canvas from "html2canvas";
+import saveSvgAsPng from "save-svg-as-png";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 // Map a group of nodes to the cellular component (organnel) they belong to and their fill color
 const GroupMapping = [
@@ -49,6 +54,7 @@ export class App extends Component {
     this.handleNodeSelected = this.handleNodeSelected.bind(this);
     this.handleFileUploaded = this.handleFileUploaded.bind(this);
     this.handleUploadedFileList = this.handleUploadedFileList.bind(this);
+    this.handleDownloadPdf = this.handleDownloadPdf.bind(this);
   }
 
   componentDidMount() {}
@@ -65,6 +71,143 @@ export class App extends Component {
 
   handleNodeSelected(node) {
     this.setState({ selectedNode: node });
+  }
+
+  handleDownloadPdf() {
+    // let imgData;
+    // const input = document.getElementById("svg_wrapper");
+    // // const input = document.getElementById("svg");
+    // // console.log(input);
+    // // input.style.width = '500px';
+    // // input.style.height = '430px';
+    // // let wrapper = document.createElement("div")
+    // // wrapper.appendChild(input);
+    // // console.log(wrapper)
+    // // document.body.appendChild(wrapper)
+
+    // html2canvas(input, {
+    //   scale: 0.5
+    // }).then(canvas => {
+    //   imgData = canvas.toDataURL("image/png");
+    //   var docDefinition = {
+    //     content: [
+    //       {
+    //         image: imgData
+    //       },
+    //       this.state.data.nodes.map(node => {
+    //         return {
+    //           style: "tableExample",
+    //           table: {
+    //             body: [
+    //               ["Name", node.id],
+    //               ["Description", node.description],
+    //               ["Organelle", node.group]
+    //             ]
+    //           },
+    //           margin: [5, 2, 10, 20]
+    //         };
+    //       })
+    //     ]
+    //   };
+
+    //   pdfMake.createPdf(docDefinition).download();
+    // });
+
+    var fonts = {
+      Roboto: {
+        normal: "fonts/Roboto-Regular.ttf",
+        bold: "fonts/Roboto-Medium.ttf",
+        italics: "fonts/Roboto-Italic.ttf",
+        bolditalics: "fonts/Roboto-MediumItalic.ttf"
+      }
+    };
+
+    let organelles = [
+      "Nucleus",
+      "Mitochondria",
+      "Golgi Apparatus",
+      "Centrosome"
+    ];
+    let prevNodeGroup = 0;
+
+    // Take the node's data add it's connections
+    let sortedData = [...this.state.data.nodes];
+    sortedData.map(node => {
+      let connectedTo = [];
+
+      this.state.data.links.map(link => {
+        if (link.source.id == node.id) {
+          if (!connectedTo.includes(link.target.id)) {
+            connectedTo.push(link.target.id);
+          }
+        }
+        if (link.target.id == node.id) {
+          if (!connectedTo.includes(link.source.id)) {
+            connectedTo.push(link.source.id);
+          }
+        }
+      });
+
+      node.connectedTo = connectedTo;
+    });
+
+    // Sort the node's data first by Group and then by connected nodes
+    sortedData.sort((a, b) => {
+      return a.group - b.group || b.connectedTo.length - a.connectedTo.length;
+
+    });
+
+    // Get the svg and return it's URI
+    saveSvgAsPng
+      .svgAsPngUri(document.getElementById("svg"), { scale: 0.55 })
+      .then(uri => {
+
+        // Used to set the PDF's content
+        var docDefinition = {
+          content: [
+            {
+              image: uri, pageBreak: 'after'
+            },
+
+            // Create content for each node
+            sortedData.map(node => {
+              
+              let nodeInfo = [
+                {
+                  // Show title for a new organelle
+                  text:
+                    prevNodeGroup != node.group
+                      ? organelles[node.group - 1]
+                      : undefined,
+                  bold: true,
+                  margin:
+                    prevNodeGroup != node.group ? [5, 12, 10, 20] : [0, 0, 0, 0]
+                },
+                {
+                  style: "tableExample",
+                  table: {
+                    body: [
+                      ["Name", node.id],
+                      ["Description", node.description],
+                      [`Connected to (${node.connectedTo.length})`, node.connectedTo.join(", ")]
+                    ]
+                  },
+                  margin: [5, 2, 10, 20]
+                }
+              ];
+
+              if (prevNodeGroup != node.group) {
+                prevNodeGroup = node.group;
+              }
+
+              return nodeInfo;
+            })
+          ]
+        };
+
+        // Download the PDF file
+        pdfMake.createPdf(docDefinition).download();
+      });
   }
 
   renderVisualization() {
@@ -110,6 +253,18 @@ export class App extends Component {
   render() {
     return this.state.data ? (
       <Fragment>
+        <div style={{ right: 15, bottom: 70, position: "absolute" }}>
+          <Button
+            id="download_pdf"
+            icon="download"
+            size={"large"}
+            type="primary"
+            shape="round"
+            onClick={this.handleDownloadPdf}
+          >
+            Download Pdf
+          </Button>
+        </div>
         <div style={{ right: 15, bottom: 15, position: "absolute" }}>
           <Button
             id="download"
